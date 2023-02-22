3f3e78a6dc8cbb06b6877ec47428cf77ead1045b
Ashley7 <ashley7520charles@gmail.com>
2023-02-22 14:07:49 +0300

    Adds backup

---
+++ app/Mail/BackupMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BackupMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = env("APP_NAME");
+
+        $database_name = env("DB_DATABASE").'.sql';     
+
+        return $this->subject($subject)->view("mail.backup")->attach('/var/www/html/back_up_file/files/'.$database_name);
+
+    }
+}
---
+++ resources/views/mail/backup.blade.php
@@ -0,0 +1,1 @@
+<p>Data backup</p>
