24cbf8eeb1c6fbbf299487b4a3be1e884bc287e6
Ashley7 <ashley7520charles@gmail.com>
2023-02-22 14:22:05 +0300

    Adds backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
    
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:backup')->everyMinute();
+        $schedule->command('send:backup')->daily();
 
     }
 
